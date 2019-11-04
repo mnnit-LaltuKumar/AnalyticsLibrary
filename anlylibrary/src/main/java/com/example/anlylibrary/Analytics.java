001a54329026689981d1524b68c0e0cf99afea3f
AL2534 <laltu.kumar@accionlabs.com>
2019-11-04 17:30:41 +0530

    Commiting the changes

--- anlylibrary/src/main/java/com/example/anlylibrary/Analytics.java
+++ anlylibrary/src/main/java/com/example/anlylibrary/Analytics.java
@@ -3,23 +3,33 @@ package com.example.anlylibrary;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.analytics.FirebaseAnalytics;
+import android.content.Context;
+import android.util.Log;
 
 public class Analytics {
 
-    public static void initialiseAnalytics(android.content.Context context,String appID,String appName){
-        android.util.Log.d("AnalyticsLib App ID= ",appID);
-        android.util.Log.d("AnalyticsLib App Name= ",appName);
+    private static FirebaseAnalytics mFirebaseAnalytics;
+
+    public static void initialiseAnalytics(Context context,String appID,String appName){
+        Log.d("AnalyticsLib App ID= ",appID);
+        Log.d("AnalyticsLib App Name= ",appName);
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setApplicationId(appID) // Required for Analytics.
                 .build();
+        Log.d("Firebase Apps= ",FirebaseApp.getApps(context).toString());
         if(FirebaseApp.getApps(context).isEmpty()) {
-            android.util.Log.d("AnalyticsLib","AnalyticsLib Initialise= ");
+            Log.d("AnalyticsLib","AnalyticsLib Initialise= ");
             FirebaseApp.initializeApp(context, options, appName);
         }
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(context);
+    }
 
-        FirebaseAnalytics analytics = FirebaseAnalytics.getInstance(context);
-        analytics.logEvent(FirebaseAnalytics.Event.APP_OPEN,null);
-        analytics.logEvent(FirebaseAnalytics.Event.LOGIN,null);
+    public static void logAppOpenEvent(){
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.APP_OPEN,null);
+    }
+
+    public static void logCustomAppEvent(android.os.Bundle bundle){
+        mFirebaseAnalytics.logEvent(FirebaseAnalytics.Event.APP_OPEN,bundle);
     }
 
 }
